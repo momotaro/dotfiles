d813a6cb1f7b5732fa808bc6ce8506eb931fde72
momotaro <m.taro.773@gmail.com>
2023-03-17 07:36:37 +0900

    Add .config/autokey

---
+++ .config/autokey/data/slack/alt-a.py
@@ -0,0 +1,3 @@
+# Enter script code
+keyboard.send_keys("<home>")
+keyboard.send_keys("<shift>+<end>")
---
+++ .config/autokey/data/vivaldi/ctrl-v.py
@@ -0,0 +1,4 @@
+# Enter script code
+import time
+cb = clipboard.get_clipboard()
+keyboard.send_keys(cb)
